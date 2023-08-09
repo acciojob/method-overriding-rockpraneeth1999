534fe27574e942914322d59b807b234518fda51e
Rock <rockpraneeth1999@gmail.com>
2023-08-09 20:26:00 +0530

    3rd time

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,8 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        A obj1=new B();
-        B obj2=new B();
-        System.out.println(obj1.meth());
-        System.out.println(obj2.meth());
+        A obj=new B();
+        System.out.println(obj.meth());
+        System.out.println(obj.meth());
     }
 }
