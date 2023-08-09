0545a64c7dd42c8820a536c43ba7fb806114bb6d
Rock <rockpraneeth1999@gmail.com>
2023-08-09 20:13:22 +0530

    Add Overriden meth() 2nd

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,9 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        B obj=new B();
-        System.out.println(obj.meth());
+        A obj1=new B();
+        B obj2=new B();
+        System.out.println(obj1.meth());
+        System.out.println(obj2.meth());
     }
 }
